12c2a4f5f1ea6d3f48533f9246c514eba0cc1107
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-08 19:50:43 -0800

    commit 2

--- server/controllers/ChatroomController.js
+++ server/controllers/ChatroomController.js
@@ -1,31 +1,33 @@
 const models = require('../models/chatroomModel');
 
-const chatroomController = {}
+const chatroomController = {};
 
 chatroomController.getMessages = async (req, res) => {
-    try {
-      const messages = await Chatroom.find();
-      res.status(200).json(messages);
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-      res.status(500).json({ error: 'Error fetching messages' });
-    }
+  try {
+    const messages = await models.Chatroom.find();
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error('Error fetching messages:', error);
+    res.status(500).json({ error: 'Error fetching messages' });
+  }
+};
+
+chatroomController.createMessage = async (req, res) => {
+  const { name, content } = req.body;
+
+  if (!name || !content) {
+    return res
+      .status(400)
+      .json({ error: 'Please provide both name and content for the message.' });
   }
 
-  chatroomController.createMessage = async (req, res) => {
-    const { name, content } = req.body;
-
-    if (!name || !content) {
-      return res.status(400).json({ error: 'Please provide both name and content for the message.' });
-    }
-
-    try {
-      const newMessage = await models.Chatroom.create({ name, content });
-      res.status(201).json(newMessage);
-    } catch (error) {
-      console.error('Error creating message:', error);
-      res.status(500).json({ error: 'Error creating message' });
-    }
+  try {
+    const newMessage = await models.Chatroom.create({ name, content });
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error('Error creating message:', error);
+    res.status(500).json({ error: 'Error creating message' });
   }
+};
 
 module.exports = chatroomController;
