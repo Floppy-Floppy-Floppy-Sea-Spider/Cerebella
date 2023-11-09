c5d5c34530e9e8c7a15df908aa5a7c1b020e82d6
yqtram <yvonneqtram@gmail.com>
2023-11-09 10:41:40 -0800

    add calendar styling

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,3 +1,42 @@
+// import React, { useState, useEffect } from 'react';
+// import "/Users/Mai/Desktop/React copy/Cerebella/client/public/calendar.css";
+
+// const Calendar = () => {
+//   const [selectedDay, setSelectedDay] = useState(null);
+//   const [dayContent, setDayContent] = useState(null);
+
+//   useEffect(() => {
+//     const fetchDayContent = async () => {
+//       if (selectedDay) {
+//         try {
+//           const response = await fetch(
+//             `http://localhost:3000/calendar/${selectedDay}`
+//           );
+//           if (response.ok) {
+//             const dayData = await response.json();
+//             setDayContent(dayData);
+//           } else {
+//             setDayContent(null);
+//           }
+//         } catch (error) {
+//           console.error('Error fetching day content:', error);
+//           setDayContent(null);
+//         }
+//       }
+//     };
+
+//     fetchDayContent();
+//   }, [selectedDay]);
+
+//   const handleSquareClick = (day) => {
+//     setSelectedDay(day);
+//   };
+
+//   const closeCard = () => {
+//     setSelectedDay(null);
+//   };
+
+//   const renderCard = () => {
 //     if (selectedDay !== null) {
 //       return (
 //         <div className="card">
@@ -91,11 +130,20 @@ const Calendar = () => {
     <div className="calendar">
       <h1 className="calTitle">Study Calendar</h1>
       {renderCard()}
-      <div className="days">
+      <div className="calendar-grid">
+        <div className="day-header">Sun</div>
+        <div className="day-header">Mon</div>
+        <div className="day-header">Tue</div>
+        <div className="day-header">Wed</div>
+        <div className="day-header">Thu</div>
+        <div className="day-header">Fri</div>
+        <div className="day-header">Sat</div>
         {[...Array(30).keys()].map((day) => (
           <div
             key={day + 1}
-            className="day-square"
+            className={`day-square ${
+              day + 1 === selectedDay ? 'selected' : ''
+            }`}
             onClick={() => handleSquareClick(day + 1)}
           >
             {day + 1}
